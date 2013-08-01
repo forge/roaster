4ce4b7c641817414100ade890787c418cb0b764d
Matt Benson <gudnabrsam@gmail.com>
2013-08-01 18:05:12 -0500

    TODO

--- api/src/main/java/org/jboss/forge/parser/java/EnumConstant.java
+++ api/src/main/java/org/jboss/forge/parser/java/EnumConstant.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.jboss.forge.parser.Internal;
 import org.jboss.forge.parser.Origin;
 
+//TODO implement MethodHolder
 public interface EnumConstant extends Internal, Origin<JavaEnum>
 {
    /**
