531893dca8b455e948c48f15f8f7201086a08304
George Gastaldi <gegastaldi@gmail.com>
2013-08-06 10:34:46 -0300

    Added test with generics from added method

--- impl/src/test/java/org/jboss/forge/test/parser/java/JavaSourceCompatibilityTest.java
+++ impl/src/test/java/org/jboss/forge/test/parser/java/JavaSourceCompatibilityTest.java
@@ -1,15 +1,23 @@
 package org.jboss.forge.test.parser.java;
 
 import org.jboss.forge.parser.JavaParser;
+import org.jboss.forge.parser.java.JavaClass;
 import org.jboss.forge.parser.java.JavaSource;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class JavaMethodBodyTest {
+public class JavaSourceCompatibilityTest {
 
 	@Test
 	public void testSupportsGenericsSource() throws Exception {
 		JavaSource<?> source = JavaParser.parse("public class Test{public void test() {java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}}}");
+		Assert.assertFalse(source.hasSyntaxErrors());
+	}
+	
+	@Test
+	public void testSupportsGenericsSourceFromMethod() throws Exception {
+		JavaClass source = JavaParser.parse(JavaClass.class,"public class Test{}");
+		source.addMethod("public void test() {java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}}");
 		Assert.assertFalse(source.hasSyntaxErrors());
 	}
 }
