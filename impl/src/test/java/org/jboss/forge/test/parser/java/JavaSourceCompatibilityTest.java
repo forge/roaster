33ef7c63f10782c148dc75475c917a925ae73baa
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2013-08-07 20:39:41 +0530

    FORGE-1096 Fixed spurious ArrayIndexOutOfBoundsException.
    The exception was thrown on rewriting the CompilationUnit, and
    was most likely caused due to use of a lower source level.
    
    Setting the source level to 1.7 gets rid of the exception.

--- impl/src/test/java/org/jboss/forge/test/parser/java/JavaSourceCompatibilityTest.java
+++ impl/src/test/java/org/jboss/forge/test/parser/java/JavaSourceCompatibilityTest.java
@@ -13,6 +13,7 @@ public class JavaSourceCompatibilityTest {
 		JavaSource<?> source = JavaParser.parse("public class Test{public void test() {java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}}}");
 		Assert.assertFalse(source.hasSyntaxErrors());
 	}
+	
 	@Test
 	public void testSupportsGenericsSourceFromConstructor() throws Exception {
 		JavaSource<?> source = JavaParser.parse("public class Test{public Test() {java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}}}");
@@ -25,4 +26,24 @@ public class JavaSourceCompatibilityTest {
 		source.addMethod("public void test() {java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}}");
 		Assert.assertFalse(source.hasSyntaxErrors());
 	}
+	
+	@Test
+   public void testSupportsGenericsSourceFromAddedConstructor() throws Exception {
+      JavaClass source = JavaParser.parse(JavaClass.class, "public class Test{}");
+      // Add a new method to get JDT to recognize the new ASTs
+      source.addMethod().setConstructor(true).setBody("java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}");
+      // Forces a rewrite to happen via AbstractJavaSource
+      source.toString();
+      Assert.assertFalse(source.hasSyntaxErrors());
+   }
+	
+	@Test
+   public void testSupportsGenericsSourceFromAddedMethod() throws Exception {
+      JavaClass source = JavaParser.parse(JavaClass.class, "public class Test{}");
+      // Add a new method to get JDT to recognize the new ASTs
+      source.addMethod().setName("test").setBody("java.util.List<String> s = new java.util.ArrayList<String>(); for (String item : s){}");
+      // Forces a rewrite to happen via AbstractJavaSource
+      source.toString();
+      Assert.assertFalse(source.hasSyntaxErrors());
+   }
 }
