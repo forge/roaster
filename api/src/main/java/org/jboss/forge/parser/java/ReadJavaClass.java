c3bb7c6d507773ec95ec321ce465cf36ce658519
Matt Benson <gudnabrsam@gmail.com>
2013-10-02 16:29:47 -0500

    eliminate JavaType

--- api/src/main/java/org/jboss/forge/parser/java/ReadJavaClass.java
+++ api/src/main/java/org/jboss/forge/parser/java/ReadJavaClass.java
@@ -10,18 +10,26 @@ package org.jboss.forge.parser.java;
 import org.jboss.forge.parser.JavaParser;
 
 /**
- * Represents a Java {@link Class} or interface source file as an in-memory modifiable element. See {@link JavaParser}
+ * Represents a Java {@link Class} source file as an in-memory modifiable element. See {@link JavaParser}
  * for various options in generating {@link ReadJavaClass} instances.
  * 
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  */
 public interface ReadJavaClass<O extends ReadJavaClass<O>> extends
-         ReadJavaType<O>,
+         ReadJavaSource<O>,
+         ReadInterfaceCapable,
+         ReadFieldHolder<O>,
+         ReadMethodHolder<O>,
+         ReadGenericCapable,
          ReadExtendable<O>,
          ReadAbstractable<O>
 {
    public interface JavaClass extends ReadJavaClass<JavaClass>,
-            JavaType<JavaClass>,
+            JavaSource<JavaClass>,
+            InterfaceCapable<JavaClass>,
+            FieldHolder<JavaClass>,
+            MethodHolder<JavaClass>,
+            GenericCapable<JavaClass>,
             Extendable<JavaClass>,
             Abstractable<JavaClass>
    {
