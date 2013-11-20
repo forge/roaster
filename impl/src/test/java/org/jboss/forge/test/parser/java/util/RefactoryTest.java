e7b24d27cdfa74b1087732e634defbf1e0c09093
George Gastaldi <gegastaldi@gmail.com>
2013-11-20 17:04:41 -0200

    Avoiding deprecated code

--- impl/src/test/java/org/jboss/forge/test/parser/java/util/RefactoryTest.java
+++ impl/src/test/java/org/jboss/forge/test/parser/java/util/RefactoryTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import java.util.List;
 
