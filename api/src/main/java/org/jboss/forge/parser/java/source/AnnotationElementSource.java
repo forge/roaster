687994a8ed02ffb1c7ffcbb9c7e8dcfca8d2e12a
Matt Benson <gudnabrsam@gmail.com>
2013-12-11 10:57:19 -0600

    copyright notices

--- api/src/main/java/org/jboss/forge/parser/java/source/AnnotationElementSource.java
+++ api/src/main/java/org/jboss/forge/parser/java/source/AnnotationElementSource.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.jboss.forge.parser.java.source;
 
 import org.jboss.forge.parser.java.AnnotationElement;
