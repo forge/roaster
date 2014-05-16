7262e1db879e102a975876a969d1c426abdde4b0
Walter Medvedeo <wmedvedeo@gmail.com>
2014-05-16 12:11:55 +0200

    SytaxError augmented to provide line and column number whenever it's possible

--- api/src/main/java/org/jboss/forge/roaster/model/SyntaxError.java
+++ api/src/main/java/org/jboss/forge/roaster/model/SyntaxError.java
@@ -18,4 +18,28 @@ public interface SyntaxError
     * Get a textual description of the type of problem encountered.
     */
    public String getDescription();
+
+    /**
+     * Gets the 1 based line number for the syntax error whenever it's possible to calculate it, or < 0 when the line is unknown.
+     */
+   public int getLine();
+
+    /**
+     * Get the 0 based column number for the error start whenever it's possible to calculate it, or < 0 when the column is unknown.
+     */
+   public int getColumn();
+
+    /**
+     * True if the syntax error is an error, false in any other case. (see isWarning())
+     * @return
+     */
+   public boolean isError();
+
+    /**
+     * True if the syntax error is a warning, false in any other case. (see isError())
+     * @return
+     */
+   public boolean isWarning();
+
+
 }
