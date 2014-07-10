f1275b1dd99e4d9d5b5ac56be56f2daf842636b6
George Gastaldi <gegastaldi@gmail.com>
2014-07-10 17:21:20 -0300

    Removed unused imports

--- impl/src/test/java/org/jboss/forge/test/roaster/model/util/RefactoryTest.java
+++ impl/src/test/java/org/jboss/forge/test/roaster/model/util/RefactoryTest.java
@@ -6,12 +6,11 @@
  */
 package org.jboss.forge.test.roaster.model.util;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.hamcrest.CoreMatchers.containsString;
 
 import java.util.List;
 
