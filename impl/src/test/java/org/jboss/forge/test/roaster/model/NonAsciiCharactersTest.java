a1c955269d2ac3dc5171e1ce476adc6b4645d782
Walter Medvedeo <wmedvedeo@gmail.com>
2014-07-17 18:38:30 +0200

    Default encoding calculation

--- impl/src/test/java/org/jboss/forge/test/roaster/model/NonAsciiCharactersTest.java
+++ impl/src/test/java/org/jboss/forge/test/roaster/model/NonAsciiCharactersTest.java
@@ -3,6 +3,7 @@ package org.jboss.forge.test.roaster.model;
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class NonAsciiCharactersTest {
@@ -19,6 +20,13 @@ public class NonAsciiCharactersTest {
 
         "}";
 
+    String nonAsciiCharactersClass2 =
+            "public class NonAsciiCharacters {" +
+
+                    "   public int punctuations_example_á = 0; " +
+
+                    "}";
+
     @Test
     public void testNonAsciiCharacters()
     {
@@ -28,6 +36,15 @@ public class NonAsciiCharactersTest {
         Assert.assertEquals( "japanese_example_現在のプロジェクトデータモデルが変更されています以下のように変更する前に保存し", javaClass.getFields().get( 1 ).getName() );
         Assert.assertEquals( "chinese_example_下面的数据对象是由外部的编辑器修改的当生成模型时对这些对象的本地修改会覆盖外部的修改", javaClass.getFields().get( 2 ).getName() );
     }
+
+    @Test
+    public void testNonAsciiCharacters2()
+    {
+        JavaClassSource javaClass = Roaster.parse( JavaClassSource.class, nonAsciiCharactersClass2 );
+        Assert.assertTrue( !javaClass.hasSyntaxErrors() );
+        Assert.assertEquals( "punctuations_example_á", javaClass.getFields().get( 0 ).getName() );
+    }
+
 
 
 }
