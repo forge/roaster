62017d8129ff2dee8491da9d5541b89894a646bd
Kai Müller <15907922+kaijMueller@users.noreply.github.com>
2019-01-25 23:39:44 +0100

    Add copyright

--- dist/src/main/java/org/jboss/forge/roaster/FormatterCommand.java
+++ dist/src/main/java/org/jboss/forge/roaster/FormatterCommand.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+*/
 package org.jboss.forge.roaster;
 
 import java.io.File;
