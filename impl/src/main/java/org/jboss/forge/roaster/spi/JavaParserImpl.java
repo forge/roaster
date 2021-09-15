4f84d7714073f157a54a34fe975a8a5240e6acc9
George Gastaldi <gegastaldi@gmail.com>
2021-09-15 12:23:24 -0300

    Avoid using deprecated attribute

--- impl/src/main/java/org/jboss/forge/roaster/spi/JavaParserImpl.java
+++ impl/src/main/java/org/jboss/forge/roaster/spi/JavaParserImpl.java
@@ -56,7 +56,7 @@ public class JavaParserImpl implements JavaParser
    public JavaUnit parseUnit(final String data)
    {
       Document document = new Document(data);
-      ASTParser parser = ASTParser.newParser(AST.JLS_Latest);
+      ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
 
       parser.setSource(document.get().toCharArray());
       parser.setCompilerOptions(JDTOptions.getJDTOptions());
