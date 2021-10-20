cd05820182b92dc31adb5cb8aaf8ca01770798e4
Asaf Ben Natan <asaf@wizzdi.com>
2021-10-20 13:09:16 +0300

    Fixes #214 - ClassLoaderWildcardImportResolver fails to find type ClassLoaderWildcardImportResolver was looking for a.b.c.*.CLASSNAME instead of a.b.c.CLASSNAME

---
+++ tests/src/test/resources/org/jboss/forge/grammar/java/MockWildcardClass.java
@@ -0,0 +1,11 @@
+package org.jboss.forge.grammar.java;
+
+import java.net.*;
+import java.util.*;
+
+public class MockWildcardClass {
+
+    public List<String> getList(){
+        return new ArrayList<>();
+    }
+}
